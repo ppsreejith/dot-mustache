acac14cabb3c55674936e9c5c35aec763ce5670d
Sreejith Pp <mailz4sreejith@gmail.com>
2017-08-07 17:06:00 +0530

    Added json parsing support

--- parser.js
+++ parser.js
@@ -4,10 +4,12 @@ function dfs(arr) {
   return (arr.length === 0) ? `"{{.}}",` : `{{#${arr[0]}}}${dfs(arr.slice(1))}{{/${arr[0]}}}`;
 }
 
+const regex = /,]/g;
+
 function parser(initString, data) {
   const keys = initString.split('.');
-  const template = dfs(keys);
-  return Mustache.render(template, data);
+  const template = `[${dfs(keys)}]`;
+  return JSON.parse(Mustache.render(template, data).replace(regex, ']'));
 }
 
 module.exports = parser;
