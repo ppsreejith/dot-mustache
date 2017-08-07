e1c262b714a03f3d19e8b13561d491b7a5301888
Sreejith Pp <mailz4sreejith@gmail.com>
2017-08-07 14:11:17 +0530

    Added a simple parser

--- parser.js
+++ parser.js
@@ -1,2 +1,13 @@
 const Mustache = require('mustache');
 
+function dfs(arr) {
+  return (arr.length === 0) ? `"{{.}}",` : `{{#${arr[0]}}}${dfs(arr.slice(1))}{{/${arr[0]}}}`;
+}
+
+function parser(initString, data) {
+  const keys = initString.split('.');
+  const template = dfs(keys);
+  return Mustache.render(template, data);
+}
+
+module.exports = parser;
