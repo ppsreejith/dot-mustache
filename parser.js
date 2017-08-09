616a3cb119e1b334145e3a74e392e69624dcfd9b
Sreejith Pp <mailz4sreejith@gmail.com>
2017-08-09 18:11:31 +0530

    Extended to support dot array syntax

--- parser.js
+++ parser.js
@@ -1,15 +1,117 @@
 const Mustache = require('mustache');
 
-function dfs(arr, index = 0) {
-  return (index === arr.length) ? `"{{{.}}}",` : `{{#${arr[index]}}}${dfs(arr, index+1)}{{/${arr[index]}}}`;
+function splitByChar(str, seperator) {
+  let bracketCounter = 0;
+  const token = '#';
+  let strBuilder = [];
+  for (const ind in str) {
+    const ch = str[ind];
+    if (bracketCounter === 0 && ch === seperator) {
+      strBuilder.push(token);
+    } else {
+      strBuilder.push(ch);
+      if (ch === '[') {
+        bracketCounter += 1;
+      } else if (ch === ']') {
+        bracketCounter -= 1;
+      }
+    }
+  }
+  return strBuilder.join('').split(token);
 }
 
-const regex = /,]/g;
+function processArray(key, index = 0) {
+  const newKey = key.slice(1, -1); // handle array here
+  const arr = splitByChar(newKey, ',');
+//  console.log("Array is", arr);
+  let jsonBuilder = [];
+  for (const ind in arr) {
+    jsonBuilder.push(`"${arr[ind]}": ${parse(arr[ind])}`);
+    if (ind !== arr.length - 1) {
+      jsonBuilder.push(',');
+    }
+  }
+  return `{${jsonBuilder.join('')}},`;
+}
+
+function dfs(arr, index = 0) {
+  const key = arr[index];
+  if (index === arr.length) {
+    return `"{{{.}}}",`;
+  }
+  if (index === arr.length - 1 && key.indexOf('[') === 0) {
+    return `${processArray(key)},`;
+  }
+  return `{{#${key}}}${dfs(arr, index + 1)}{{/${key}}}`;
+}
+
+function parse(str) {
+  let keys = splitByChar(str, '.');
+//  console.log("Keys are", keys);
+  return `[${dfs(keys)}]`;
+}
+
+function fixTemplate(template) {
+  const patterns = [
+    {
+      regex: /,+/g,
+      replace: ',',
+    },
+    {
+      regex: /,]/g,
+      replace: ']',
+    },
+    {
+      regex: /,}/g,
+      replace: '}',
+    },
+    {
+      regex: /}\"/g,
+      replace: '}',
+    },
+    {
+      regex: /\"{/g,
+      replace: '{',
+    }
+  ];
+  for (let ind in patterns) {
+    const regex = patterns[ind].regex;
+    const replace = patterns[ind].replace;
+    template = template.replace(regex, replace);
+  }
+  return template;
+}
 
 function parser(initString, data) {
-  const keys = initString.split('.');
-  const template = `[${dfs(keys)}]`;
-  return JSON.parse(Mustache.render(template, data).replace(regex, ']'));
+  const template = parse(initString);
+  const rendered = Mustache.render(template, data);
+  console.log('Template is', rendered);
+  return JSON.parse(fixTemplate(rendered));
+}
+
+function validator(str) {
+  let bracketCounter = 0;
+  for (const ind in str) {
+    const ch = str[ind];
+    if (ch === ' ') {
+      return false; // should contain space
+    }
+    if (ch === '[') {
+      if (str[ind - 1] !== '.') {
+        return false; // array should be property of an object
+      }
+      bracketCounter += 1;
+    } else if (ch === ']') {
+      bracketCounter -= 1;
+    }
+    if (bracketCounter < 0) {
+      return false; // Imbalanced brackets
+    }
+  }
+  if (bracketCounter === 0) { // Imbalanced brackets
+    return true;
+  }
+  return false;
 }
 
 module.exports = parser;
--- test.js
+++ test.js
@@ -1,17 +1,38 @@
 const parser = require('./parser');
 
-let output = parser('body.text.crit.words', {
+let output = parser('body.text.crit.[a.b,b]', {
   body: {
-    text: [{
-      crit: {
-        words: ['This works', 'This too'],
-      },
-    }, {
-      crit: [{
-        words: {title: 'this is an object', toString: function() {return JSON.stringify(this).replace(/"/g, '\\\"')}},
+    text: [
+      {
+        crit: {
+          a: {
+            b: 'lol',
+            toString: function() { return JSON.stringify(this); },
+          },
+          b: {
+            b: 'na',
+            toString: function() { return JSON.stringify(this); },
+          },
+          toString: function() { return JSON.stringify(this); },
+        },
+        toString: function() { return JSON.stringify(this); },
+      }, {
+        crit: {
+          a: {
+            b: 'ha lol',
+            toString: function() { return JSON.stringify(this); },
+          },
+          b: {
+            b: 'ha na',
+            toString: function() { return JSON.stringify(this); },
+          },
+          toString: function() { return JSON.stringify(this); },
+        },
+        toString: function() { return JSON.stringify(this); },
       }],
-    }],
   },
 });
 
-console.log("Output is", output);
+console.log("Output is", JSON.stringify(output));
+
+console.log("Output should be", '[{a.b: "lol", b: {b: "na"}}, {a.b: "ha lol", b: {b: "ha na"}}]');
